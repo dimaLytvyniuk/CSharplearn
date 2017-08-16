4187eb3f42b28d73682e37ac396bae113fbb85c3
Dima Lytvyniuk <wppresent@outlook.com>
2017-08-16 10:06:27 +0300

    Add QuickSort

---
+++ Data Structers and Algorithm/SortingMethods/SortingMethods/Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingMethods
+{
+    class Methods
+    {
+        private static void swap(int[] a, int i, int j)
+        {
+            int prom = a[i];
+            a[i] = a[j];
+            a[j] = prom;
+        }
+
+        public static void QuickSort(int[] a, int low, int high)
+        {
+            if (high > low)
+            {
+                int pivot = Partition(a, low, high);
+                QuickSort(a, low, pivot - 1);
+                QuickSort(a, pivot + 1, high);
+            }
+        }
+
+        private static int Partition(int[] a, int low, int high)
+        {
+            int pivot_index = ((high - low) / 2) + low;
+            int wall = low;
+            int pivot = a[pivot_index];
+            swap(a, pivot_index, high);
+
+            for (int i = low; i < high; i++)
+            {
+                if (a[i] <= pivot)
+                {
+                    swap(a, wall, i);
+                    wall++;
+                }
+            }
+
+            swap(a, wall, high);
+            return wall;
+        }
+    }
+}
---
+++ Data Structers and Algorithm/SortingMethods/SortingMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int count = 100000;
+            int[] a = new int[count];
+
+            for (int i = 0; i < count; i++)
+                a[i] = random.Next(100000);
+
+            for (int i = 0; i < count; i++)
+                Console.Write(a[i] + " ");
+
+            Methods.QuickSort(a, 0, count - 1);
+
+            Console.WriteLine();
+
+            for (int i = 0; i < count - 1; i++)
+                Console.Write(a[i] + " ");
+        }
+    }
+}
