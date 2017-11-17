365ce9dfaaa5df40345e5a3a8f48dcc867d10d52
Dima Lytvyniuk <wppresent@outlook.com>
2017-11-17 18:54:36 +0200

    Add CodeWars

---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factors(7775460));
+        }
+
+        public static String factors(int lst)
+        {
+            Dictionary<int, int> factors = new Dictionary<int, int>();
+            String result = "";
+            int num;
+
+            while ((num = NextPrime(lst)) != lst)
+            {
+                if (factors.ContainsKey(num))
+                    factors[num]++;
+                else
+                    factors[num] = 1;
+
+                lst /= num;
+            }
+
+            if (factors.ContainsKey(num))
+                factors[num]++;
+            else
+                factors[num] = 1;
+
+            foreach (int keys in factors.Keys)
+            {
+                if (factors[keys] > 1)
+                    result += $"({keys}**{factors[keys]})";
+                else
+                    result += $"({keys})";
+            }
+
+            return result;
+
+        }
+
+        public static int NextPrime(int number)
+        {
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                    return i;
+            }
+
+            return number;
+        }
+    }
+}
