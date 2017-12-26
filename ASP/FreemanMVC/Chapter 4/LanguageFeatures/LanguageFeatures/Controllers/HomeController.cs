0eeeef9fac3ab457f7f630140f8e53bc8aecebfb
Dima Lytvyniuk <wppresent@outlook.com>
2017-12-26 20:07:32 +0200

    End Freeman Pro Asp Chapter 4.

---
+++ ASP/FreemanMVC/Chapter 4/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ViewResult> Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"{nameof(p.Name)}: {name}, Price: {price}, Related: {relatedName}");
+            }
+
+            long? length = await MyAsyncMethods.GetPageLength() ?? 0;
+            return View(results);
+        }
+    }
+}
---
+++ ASP/FreemanMVC/Chapter 4/LanguageFeatures/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart shoppingCart)
+        {
+            decimal total = 0;
+            foreach (Product prod in shoppingCart.Products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+    }
+}
