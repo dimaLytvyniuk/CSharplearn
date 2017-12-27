a1d627a5571a338630de6485b44575b32b33ae75
Dima Lytvyniuk <wppresent@outlook.com>
2017-12-27 21:04:18 +0200

    End Freeman Pro Asp Chapter 5.

---
+++ ASP/FreemanMVC/Chapter 5/Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            ViewBag.StockLevel = 4;
+
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Corner flag", Price = 34.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M}
+            };
+
+            ViewBag.ResultArray = array;
+
+            return View(myProduct);
+        }
+    }
+}
