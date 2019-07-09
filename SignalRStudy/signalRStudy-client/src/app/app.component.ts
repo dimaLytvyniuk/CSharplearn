f79f4c6c67494da466733a011db3e483c38561c8
LytvyniukDima <dima.lytvyniuk@gmail.com>
2019-07-09 18:20:56 +0300

    Add base functionality for sending and receiving messages

--- SignalRStudy/signalRStudy-client/src/app/app.component.ts
+++ SignalRStudy/signalRStudy-client/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalRService } from './services/signal-r.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signalRStudy-client';
+
+  constructor(public signalRService: SignalRService) { }
+
+  ngOnInit() {
+    this.signalRService.startConnection();
+    //this.signalRService.sendMessage();   
+    this.signalRService.receveiveMessage();
+  }
 }
